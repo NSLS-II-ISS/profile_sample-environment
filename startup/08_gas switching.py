6a8692d85b9016dcbd12a0c79c451b1f3803aef6
elistavitski <elistavitski@gmail.com>
2023-06-30 12:01:15 -0400

    added switching valves

--- startup/08_gas switching.py
+++ startup/08_gas switching.py
@@ -1,12 +1,14 @@
-'''
-
 
 class SwitchValve(Device):
-    open = Cpt(EpicsSignal, 'Cmd:Opn-Cmd')
+    state = Cpt(EpicsSignal, -Sel)
 
-switch_valve_1 = EpicsSignal('XF:08IDB-CT{DIODE-Box_B1:1}Out:1-Sel', name='switch_valve_1')
-switch_valve_2 = EpicsSignal('XF:08IDB-CT{DIODE-Box_B1:1}Out:1-Sel', name='switch_valve_1')
-switch_valve_3 = EpicsSignal('XF:08IDB-CT{DIODE-Box_B1:1}Out:1-Sel', name='switch_valve_1')
-ghs_ch1_reactor = EpicsSignal('XF:08IDB-UT{Gas:1-Vlv:PSV0201}Cmd', name='ghs_ch1_reactor')
-XF:08IDB-CT{DIODE-Box_B1:1}Out:1-Sel
-'''
+    def to_reactor():
+        state.set(1)
+
+    def to_exhaust():
+        state.set(0)
+
+
+switch_valve_1 = SwitchValve('XF:08IDB-CT{DIODE-Box_B1:1}Out:0-', name='switch_valve_1')
+switch_valve_2 = SwitchValve('XF:08IDB-CT{DIODE-Box_B1:1}Out:1', name='switch_valve_1')
+switch_valve_3 = SwitchValve('XF:08IDB-CT{DIODE-Box_B1:1}Out:2', name='switch_valve_1')
